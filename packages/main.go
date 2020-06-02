89524d71c60ddaac1ba805bba061d3c9a24997bb
abidurr <42abid@gmail.com>
2020-06-02 18:43:46 -0400

    Creating your own package

--- packages/main.go
+++ packages/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"math"
+	"github.com/abidurr/go-playground/packages/strutil"
 )
 
 func main() {
+
 	fmt.Println(math.Floor(1.23))
 	fmt.Println(math.Ceil(1.23))
 	fmt.Println(math.Sqrt(121))
+
+	fmt.Println(strutil.Reverse("!olleH"))
+
 }
